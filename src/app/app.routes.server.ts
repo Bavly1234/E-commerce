6958b9fd6a8dbab32abb4392f9386a59a3cf3ac5
Bavly Maged <pavelymaged5@gmail.com>
2026-04-27 18:12:48 +0300

    Fix SSR prerender and navbar categories

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,36 @@
+// import { RenderMode, ServerRoute } from '@angular/ssr';
+
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender
+//   }
+// ];
+
+
+
+
+
+
+
+
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'brandDetails/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'details/:id/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
